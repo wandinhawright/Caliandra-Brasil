1db95cacfd32a582534ad1c713cb1a5d33e4a62e
wandinhawright <wwanda.wwright@gmail.com>
2026-01-23 17:13:39 -0300

    alterações

--- principal/caliandra/caliandra/settings.py
+++ principal/caliandra/caliandra/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
